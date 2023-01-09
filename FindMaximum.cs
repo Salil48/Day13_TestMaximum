b0f8f30fbb1c516cfcdc0b711f1c92e1fe697f86
“Salil48” <salilchharia@gmail.com>
2023-01-09 19:20:26 +0530

    Extend the max method to take more then three parameters

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -6,38 +6,33 @@ using System.Threading.Tasks;
 
 namespace GenericsTestMax
 {
-    public class FindMaximum
-
+    public class FindMaximum<T> where T : IComparable
     {
-        public class testCases<T> where T : IComparable
+        public T[] value;
+
+        public FindMaximum(T[] value)
         {
-            public T[] value;
-
-            public testCases(T[] value)
-            {
-                this.value = value;
-            }
-            public T[] Sort(T[] values)
-            {
-                Array.Sort(values);
-                return values;
-            }
-            public T Maxvalue(params T[] values)
-            {
-                var sort_val = Sort(values);
-                return sort_val[^1];
-            }
-            public T Maxmethod()
-            {
-                var max = Maxvalue(this.value);
-                return max;
-            }
-            public void toPrint()
-            {
-                var max = Maxvalue(this.value);
-                Console.WriteLine("The Maximum value is :" + max);
-            }
+            this.value = value;
         }
-
+        public T[] Sort(T[] values)
+        {
+            Array.Sort(values);
+            return values;
+        }
+        public T Maxvalue(params T[] values)
+        {
+            var sort_val = Sort(values);
+            return sort_val[^1];
+        }
+        public T Maxmethod()
+        {
+            var max = Maxvalue(this.value);
+            return max;
+        }
+        public void PrintMaxValue()
+        {
+            var max = Maxvalue(this.value);
+            Console.WriteLine("The Maximum value is :" + max);
+        }
     }
 }
