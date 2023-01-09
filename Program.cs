d3738c023e304db6c9577834b7c9ef6cdae6fb03
“Salil48” <salilchharia@gmail.com>
2023-01-09 15:35:18 +0530

    Given 3 Integers find the Maximum

--- Program.cs
+++ Program.cs
@@ -6,7 +6,14 @@
         {
             Console.WriteLine("Welcome to Test Maximum Problem using Generics");
 
-            
+            int first = 30;
+            int second = 50;
+            int third = 100;
+
+            int val = FindMaximum.MaximumIntegerNumber(first, second, third);
+
+            Console.WriteLine(" The Maximum integer is " + val);
+
         }
     }
 }
