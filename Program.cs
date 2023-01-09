ff6503b5ee594f1b57e9a4c836d8f9d1a1d5478b
“Salil48” <salilchharia@gmail.com>
2023-01-09 18:59:42 +0530

    Extend the max method to take more then three parameters

--- Program.cs
+++ Program.cs
@@ -1,4 +1,6 @@
-﻿namespace GenericsTestMax
+﻿using static GenericsTestMax.FindMaximum;
+
+namespace GenericsTestMax
 {
     public class Program
     {
@@ -6,14 +8,54 @@
         {
             Console.WriteLine("Welcome to Test Maximum Problem using Generics");
 
-            string first = "Apple";
-            string second = "Banana";
-            string third = "Peach";
+            Console.WriteLine("Enter which datatype you want 1)Integer 2)Float 3)String :");
+            int z = Convert.ToInt32(Console.ReadLine());
 
-            string val = FindMaximum.MaximumString(first, second, third);
+            switch (z)
+            {
+                case 1:
+                    Console.WriteLine("Enter the number of elements in array: ");
+                    int n = Convert.ToInt32(Console.ReadLine());
+                    int[] arr = new int[n];
+                    for (int i = 0; i < n; i++)
+                    {
+                        Console.WriteLine("Enter the number  {0} : ", i);
+                        arr[i] = Convert.ToInt32(Console.ReadLine());
+                    }
+                    testCases<int> test = new testCases<int>(arr);
+                    test.toPrint();
+                    break;
 
-            Console.WriteLine(" The Maximum string character is " + val);
+                case 2:
+                    Console.WriteLine("Enter the number of elements in array: ");
+                    int n1 = Convert.ToInt32(Console.ReadLine());
+                    double[] arr1 = new double[n1];
+                    for (int i = 0; i < n1; i++)
+                    {
+                        Console.WriteLine("Enter the number  {0} : ", i);
+                        arr1[i] = Convert.ToDouble(Console.ReadLine());
+                    }
+                    testCases<double> test1 = new testCases<double>(arr1);
+                    test1.toPrint();
+                    break;
 
+                case 3:
+                    Console.WriteLine("Enter the number of elements in array: ");
+                    int n2 = Convert.ToInt32(Console.ReadLine());
+                    string[] arr2 = new string[n2];
+                    for (int i = 0; i < n2; i++)
+                    {
+                        Console.WriteLine("Enter the string  {0} : ", i);
+                        arr2[i] = Console.ReadLine();
+                    }
+                    testCases<string> test2 = new testCases<string>(arr2);
+                    test2.toPrint();
+                    break;
+
+            }
         }
+
+
     }
 }
+
