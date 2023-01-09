21414e8074d27c29e2eeb99f946d670aa26e849c
“Salil48” <salilchharia@gmail.com>
2023-01-09 19:33:35 +0530

    Extend the max method to also print the max to std out using Generic Method

--- FindMaximum.cs
+++ FindMaximum.cs
@@ -6,33 +6,39 @@ using System.Threading.Tasks;
 
 namespace GenericsTestMax
 {
-    public class FindMaximum<T> where T : IComparable
+    public class FindMaximum
     {
-        public T[] value;
+        public class Maximum<T> where T : IComparable
+        {
+            public T[] value;
 
-        public FindMaximum(T[] value)
-        {
-            this.value = value;
+            public Maximum(T[] value)
+            {
+                this.value = value;
+            }
+            public T[] Sort(T[] values)
+            {
+                Array.Sort(values);
+                return values;
+            }
+            public T Maxvalue(params T[] values)
+            {
+                var sort_val = Sort(values);
+                return sort_val[^1];
+            }
+            public T Maxmethod()
+            {
+                var max = Maxvalue(this.value);
+                return max;
+            }
+            public void PrintMaxValue()
+            {
+                var max = Maxvalue(this.value);
+                Console.WriteLine("The Maximum value is :" + max);
+            }
+
         }
-        public T[] Sort(T[] values)
-        {
-            Array.Sort(values);
-            return values;
-        }
-        public T Maxvalue(params T[] values)
-        {
-            var sort_val = Sort(values);
-            return sort_val[^1];
-        }
-        public T Maxmethod()
-        {
-            var max = Maxvalue(this.value);
-            return max;
-        }
-        public void PrintMaxValue()
-        {
-            var max = Maxvalue(this.value);
-            Console.WriteLine("The Maximum value is :" + max);
-        }
+
+
     }
 }
