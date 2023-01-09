63029182d2e6c1c90d623904d49f2c5616facec9
“Salil48” <salilchharia@gmail.com>
2023-01-09 15:34:19 +0530

    Given 3 Integers find the Maximum

---
+++ FindMaximum.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace GenericsTestMax
+{
+    public class FindMaximum
+
+    {
+        public static int MaximumIntegerNumber(int firstValue,  int SecondValue,  int thirdValue)
+        {
+            if (firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(thirdValue) > 0 ||
+                firstValue.CompareTo(SecondValue) >= 0 && firstValue.CompareTo(thirdValue) >= 0 ||
+                firstValue.CompareTo(SecondValue) > 0 && firstValue.CompareTo(thirdValue) >= 0)
+            {
+                return firstValue;
+            }
+
+            if (SecondValue.CompareTo(firstValue) > 0 && SecondValue.CompareTo(thirdValue) > 0 ||
+                SecondValue.CompareTo(firstValue) >= 0 && SecondValue.CompareTo(thirdValue) >= 0 ||
+                SecondValue.CompareTo(firstValue) < 0 && SecondValue.CompareTo(thirdValue) < 0)
+            {
+                return SecondValue;
+            }
+
+            if (thirdValue.CompareTo(SecondValue) > 0 && thirdValue.CompareTo(firstValue) > 0 ||
+                thirdValue.CompareTo(SecondValue) >= 0 && thirdValue.CompareTo(firstValue) >= 0 ||
+                thirdValue.CompareTo(SecondValue) > 0 && thirdValue.CompareTo(firstValue) >= 0)
+            {
+                return thirdValue;
+            }
+
+            return firstValue;
+        }
+        
+    }
+}
